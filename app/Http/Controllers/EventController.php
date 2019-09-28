54ac6a2849526d0e6fed11f436fd65176f6a7c23
agoui <1135567254@qq.com>
2019-09-28 10:48:19 +0800

    ww

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -114,7 +114,7 @@ class EventController extends Controller
                     'add_time'=>time()
                 ]);
             }
-            $message = '欢迎'.$user_info['nickname'].'同学，感谢您的关注';
+            $message = '欢迎'.$user_info['nickname'];
             $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
             echo $xml_str;
 ///
